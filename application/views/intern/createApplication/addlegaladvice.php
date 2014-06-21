4f61d78d01a6f6d3366a5b12ddddc0e9b71572dc
hottestdeveloper <irene_indiongco@dlsu.ph>
2014-06-21 12:26:29 +0800

    createApplication/addlegaladvice

--- application/views/intern/createApplication/addlegaladvice.php
+++ application/views/intern/createApplication/addlegaladvice.php
@@ -1,94 +1,168 @@
 <div id='legaladvice_form' class="container">
-    <div class="row">
-        <br>
-        <div class="col-sm-2 control-group">
+
+	<div class="row">
+
+		<div class="col-sm-2 control-group">
+			<div class="controls">
+				<center> <h5> <b> Application Form No. </b></h5> </center>
+			</div>
+		</div>
+
+		<div class="col-sm-1 control-group">
+			<div class="controls" style='margin-top:2px;'>
+				<h5><?= $appnumber ?></h5>
+			</div>
+		</div>
+
+		<div class="col-sm-5 control-group"></div>
+
+		<div class="col-sm-3 control-group">
             <div class="controls">
-                <center> <h5> <b>Supervising Lawyer</b> </h5> </center>
+                <center> <h5> <b>
+                    <div class="clientnamediv">
+                        <?php
+                        $newclientid = $this->session->userdata('newclientid');
+                        if(isset($newclientid))
+                            echo $this->People_model->getuserfield('lastname', $newclientid) . ', ' . $this->People_model->getuserfield('firstname', $newclientid) . ' ' . substr($this->People_model->getuserfield('middlename', $newclientid), 0, 1) . '.';
+                        else{
+                            $first = 0;
+                            foreach ($clientlist as $row) {
+                                if ($first == 0)
+                                    echo $row->lastname . ', ' . $row->firstname . ' ' . substr($row->middlename, 0, 1) . '.';
+                                $first++;
+                            }
+                        }
+                        ?>
+                    </div>
+                </b></h5> </center>
             </div>
         </div>
 
-        <div class="col-sm-9 control-group">
-            <div class="controls">
-                <h5> <?php if(isset($supervising->firstname)) echo "$supervising->firstname $supervising->lastname"; else echo "None."; ?> </h5>
-            </div>
-        </div>
-    </div>
+	<div class="col-sm-1 control-group">
+		<div class="controls">
 
-    <div class="row">
-        <br>
-        <div class="col-sm-2 control-group">
-            <div class="controls">
-                <center> <h5> <b>Advice Given by the Intern</b> </h5> </center>
-            </div>
-        </div>
-
-        <div class="col-sm-9 control-group">
-            <div class="controls">
-                <h5 style="line-height:20px;"><?php echo $case->appAdviceGiven ?></h5>
-            </div>
-        </div>
-    </div>
+		</div>
+	</div>
+</div><br>
 
 
-    <br>
-    <div class="row">
-        <div class="col-sm-2 control-group">
-            <div class="controls">
-                <center> <h5> <b>Notes</b> </h5> </center>
-            </div>
-        </div>
+<div class="row" style='padding-left:20px; padding-right:25px;'>
 
-        <div class="col-sm-9 control-group">
-            <div class="controls">
-                <h5 style="line-height:20px;"><?php echo $case->appNotes ?></h5>
-            </div>
-        </div>
-    </div>
-    <br>
 
-    <div class="row">
-        <div class="col-sm-2 control-group">
-            <div class="controls">
-                <center> <h5> <b>Interviewers</b> </h5> </center>
-            </div>
-        </div>
+	<div class="col-sm-2 control-group">
+		<div class="controls">
+			<center> <h5> <b>Supervising Lawyer </b></h5> </center>
+		</div>
+	</div>
 
-        <div class="col-sm-9 control-group">
-            <div class="controls">
-                <?php foreach ($interviewee as $i) : ?>
-                <h5>
-                    <?php echo "$i->firstname $i->lastname" ?>
-                </h5><?php endforeach; ?>
-            </div>
-        </div>
-    </div>
+	<div class="col-sm-3 control-group">
+		<div class="controls">
+			<select id="applawyer" name="applawyer" class="form-control">
+				<option></option>
+				<?php foreach ($lawyerlist as $row) : ?>
+				<option value="<?= $row->personID ?>">
+					<?= $row->lastname ?>, <?= $row->firstname ?> <?= substr($row->middlename, 0, 1) ?>.
+				</option><?php endforeach; ?>
+			</select>
+		</div>
+	</div>
+
+	<br><br><br>
+
+	<div class="col-sm-2 control-group">
+		<div class="controls">
+			<center> <h5> <b>Other Interviewers</b> </h5> </center>
+		</div>
+	</div>
+
+	<div class="col-sm-3 control-group">
+		<div class="controls interviewees-table">
+			<div class="tbl-attendees">
+				<table class="table table-striped">
+					<?php foreach ($internlist as $row) : if ($row->personID != $this->session->userdata('userid')) { ?>
+					<tr>
+						<td align="center">
+							<input type= 'checkbox' name='otherinterviewers[]' class= 'case' name= 'case' value= '<?php echo $row->personID ?>'/>
+						</td>
+						<td>
+							<?php echo $row->firstname . ' ' . $row->lastname; ?>
+						</td>
+					</tr>
+					<?php } endforeach; ?>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	<br><br><br><br><br><br><br><br><br>
+
+	<div class="col-sm-2 control-group">
+		<div class="controls">
+			<center> <h5> <b>Advice Given by the Intern</b> </h5> </center>
+		</div>
+	</div>
+
+	<div class="col-sm-9 control-group">
+		<div class="controls">
+			<?php echo form_textarea(array('id' => 'appadvicegiven', 'name' => 'appadvicegiven', 'type' => 'text', 'class' => 'form-control', 'style' => 'height: 150px')); ?>
+		</div>
+	</div>
+	<br><br><br><br><br><br><br><br><br><br>
+
+	<div class="col-sm-2 control-group">
+		<div class="controls">
+			<center> <h5> <b>Notes </b></h5> </center>
+		</div>
+	</div>
+
+	<div class="col-sm-9 control-group">
+		<div class="controls">
+			<?php echo form_textarea(array('id' => 'appnotes', 'name' => 'appnotes', 'type' => 'text', 'class' => 'form-control', 'style' => 'height: 100px')); ?>
+		</div>
+	</div>
+
+</div>
+<br>
+
 </div>
 
-<div id='recommendation_form' class="container">
-    <br>
-    <div class="row">
 
-        <h4>Intern's Recommendation for the Director</h4>
-        <hr width='1150px'>
-        <br>
+<div class="row" id='recommendation_form'>
 
-        <div class="col-sm-2 control-group">
-            <div class="controls">
-                <center> <h5> <b>Recommended for</b> </h5> </center>
-            </div>
-        </div>
+<h5><b>Intern's Recommendation for the Director</b></h5>
+<hr width='950px'>
 
-        <div class="col-sm-9 control-group">
-            <h5 style="line-height:20px;">
-                <?php echo "$case->appRecommendedFor <br><br><br> $case->appRecommendation" ?>
-            </h5>
-        </div>
+<div class="col-sm-2 control-group">
+	<div class="controls">
+		<center> <h5> <b>Recommended for</b> </h5> </center>
+	</div>
+</div>
 
-    </div>
-    <br><br>
+<!-- Multiple Radios -->
+<div class="control-group">
+	<div class="controls">
+		<?php echo form_radio(array('name' => 'apprecommendedfor', 'id' => 'recommendedFor-0', 'value' => 'Approval', 'checked' => TRUE)); ?>
+		Approval
+		<?php echo form_radio(array('name' => 'apprecommendedfor', 'id' => 'recommendedFor-1', 'value' => 'Rejection', 'checked' => FALSE)); ?>
+		Rejection
+	</div>
+</div>
+
+<br>
+
+<div class="col-sm-2 control-group">
+	<div class="controls">
+		<center> <h5> <b>Rationale</b> </h5> </center>
+	</div>
+</div>
+
+<div class="col-sm-9 control-group">                  
+	<?php echo form_textarea(array('id' => 'apprecommendation', 'name' => 'apprecommendation', 'type' => 'text', 'class' => 'form-control', 'style' => 'height: 280px' )); ?>
+</div>
 
 </div>
 
+<!-- Button -->
 	<!-- Button -->
 	<div class="row">
 		<div class="control-group pull-right pull-down">
@@ -97,3 +171,4 @@
 			</div>
 		</div>
 	</div>
+
