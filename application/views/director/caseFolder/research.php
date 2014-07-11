5a792e3869e7041bab5c90d0a2b539f1b7e7641b
loooweeen <psP2014>
2014-07-11 15:45:30 +0800

    Fixed last commit

--- application/views/director/caseFolder/research.php
+++ application/views/director/caseFolder/research.php
@@ -6,17 +6,20 @@
         <ul class="nav nav-tabs">
             <li class="active"><a href="#newResearch" data-toggle="tab">New</a></li>
             <?php foreach ($caseresearch as $row): ?>
-                <li><a href="#currentResearch" data-toggle="tab" ><?php echo $row->caseName?></a></li>
+                <li><a href="#currentResearch" data-toggle="tab" ><?php echo $row->caseName ?></a></li>s
             <?php endforeach; ?>
         </ul>
-        <div class="tab-content" style="height:450px; overflow:scroll;">
+        <div class="tab-content" style="height:400px; overflow:scroll ! important;">
             <div class="tab-pane active" id="newResearch">
                 <?php $this->load->view('intern/research/newResearch'); ?>
             </div>
-            <div class="tab-pane" id="currentResearch">
-                <?php $this->load->view('intern/research/currentResearch'); ?>
-            </div>
+            <?php foreach ($caseresearch as $row): ?>
+                <div class="tab-pane" id="currentResearch<?php echo $row->caseID ?>">
+                    <?php $data['currentresearch'] = $row ?>
+                    <?php $data['files'] = $this->Case_model->select_casedocuments($row->caseID, 2) ?>
+                    <?php $this->load->view('intern/research/currentResearch', $data); ?>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
-
 </div>
