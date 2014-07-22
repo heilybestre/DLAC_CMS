186b66456f7f702df0b9204093c4aaf0f41474c3
heilymamenta <mihaelamamenta@192.168.1.109>
2014-07-22 18:05:40 +0800

    Fixed view application (for intern, lawyer, director, secretary) - removed unnecessary files - removed intern's recommendation - fixed UI (OC much)

--- application/views/director/viewApplication/viewlegaladvice.php
+++ application/views/director/viewApplication/viewlegaladvice.php
@@ -63,28 +63,3 @@
         </div>
     </div>
 </div>
-
-<div id='recommendation_form' class="container">
-    <br>
-    <div class="row">
-
-        <h4>Intern's Recommendation for the Director</h4>
-        <hr width='1150px'>
-        <br>
-
-        <div class="col-sm-2 control-group">
-            <div class="controls">
-                <center> <h5> <b>Recommended for</b> </h5> </center>
-            </div>
-        </div>
-
-        <div class="col-sm-9 control-group">
-            <h5 style="line-height:20px;">
-                <?php echo "$case->appRecommendedFor <br><br><br> $case->appRecommendation" ?>
-            </h5>
-        </div>
-
-    </div>
-    <br><br>
-
-</div>
--- application/views/secretary/viewApplication/viewlegaladvice.php
+++ application/views/secretary/viewApplication/viewlegaladvice.php
@@ -1,58 +1,65 @@
-<div id='view_actiontaken' class="container">
-
-    <?php echo form_open(base_url() . 'viewapplication/legaladvice', array('class' => 'form-horizontal')); ?>
-
-    <div class="row-fluid">
+<div id='legaladvice_form' class="container">
+    <div class="row">
         <br>
-
-        <!-- Textarea -->
-        <div class="control-group">
-            <label class="control-label" for="adviceGiven">Advice Given by the Intern</label>
-            <div class="controls">                     
-                <label class="control-label1">(advice)</label>		
+        <div class="col-sm-2 control-group">
+            <div class="controls">
+                <center> <h5> <b>Supervising Lawyer</b> </h5> </center>
             </div>
         </div>
 
+        <div class="col-sm-9 control-group">
+            <div class="controls">
+                <h5> <?php if(isset($supervising->firstname)) echo "$supervising->firstname $supervising->lastname"; else echo "None."; ?> </h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <br>
+        <div class="col-sm-2 control-group">
+            <div class="controls">
+                <center> <h5> <b>Advice Given by the Intern</b> </h5> </center>
+            </div>
+        </div>
+
+        <div class="col-sm-9 control-group">
+            <div class="controls">
+                <h5 style="line-height:20px;"><?php echo $case->appAdviceGiven ?></h5>
+            </div>
+        </div>
+    </div>
+
+
+    <br>
+    <div class="row">
+        <div class="col-sm-2 control-group">
+            <div class="controls">
+                <center> <h5> <b>Notes</b> </h5> </center>
+            </div>
+        </div>
+
+        <div class="col-sm-9 control-group">
+            <div class="controls">
+                <h5 style="line-height:20px;"><?php echo $case->appNotes ?></h5>
+            </div>
+        </div>
     </div>
     <br>
-    <div class = "row-fluid">
 
-        <!-- Textarea -->
-        <div class="control-group">
-            <label class="control-label" for="notes">Notes</label>
-            <div class="controls">   
-                <label class="control-label1">(notes)</label>                  
+    <div class="row">
+        <div class="col-sm-2 control-group">
+            <div class="controls">
+                <center> <h5> <b>Interviewers</b> </h5> </center>
             </div>
         </div>
 
-        <!-- Button
-        <div class="control-group">
-                <label class="control-label" for="submit"></label>
-                <div class="controls">
-        <?php echo form_submit(array('name' => 'submit', 'class' => 'btn btn-primary'), 'Next'); ?>
-                </div>
-        </div>
-        -->
-    </div> <br>
-
-    <div class="row-fluid">
-
-
-        <div class="span4">
-
-            <div class="control-group">
-                <label class="control-label" for="notes">Other Interviewers</label>
-                <div class="controls">
-                    <label class="control-label1">Jal Laylo, David Guetta </label> 
-
-                </div>
+        <div class="col-sm-9 control-group">
+            <div class="controls">
+                <?php foreach ($interviewee as $i) : ?>
+                <h5>
+                    <?php echo "$i->firstname $i->lastname" ?>
+                </h5><?php endforeach; ?>
             </div>
         </div>
-
     </div>
-
-
-    <?php echo form_close(); ?>
-
 </div>
-
