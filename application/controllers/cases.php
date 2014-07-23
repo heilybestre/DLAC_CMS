0bcb06667d7a480b3608eb808a436a6148dcf210
heilymamenta <mihaelamamenta@192.168.1.109>
2014-07-23 11:18:29 +0800

    Revert "Revert "Displayed client name in add new appointment modal""
    This reverts commit 0f2541396efe3b94281a9d922c9fe589581f3899.

--- application/controllers/cases.php
+++ application/controllers/cases.php
@@ -942,7 +942,7 @@ class Cases extends CI_Controller {
           $datenow = date("Y-m-d", now());
           $uid = $this->session->userdata('userid');
 
-          $deadline = date('Y-m-d', strtotime("+$newdocumentdeadline[$count] days"));
+          $deadline = date('Y-m-d', strtotime("+$newdocumentdeadline[$count] weekdays"));
 
           $newdeadline = array(
               'caseID' => $cid,
