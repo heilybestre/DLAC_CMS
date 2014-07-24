b037f1ab7ac84319950f09979b3bd3062ac2a06c
heilymamenta <mihaelamamenta@192.168.1.109>
2014-07-24 12:43:57 +0800

    Bye bye bootstrap validator ☹

--- application/views/loginbody.php
+++ application/views/loginbody.php
@@ -1,32 +1,33 @@
 <!-- <img src="<?= base_url() ?>assets/img/5.jpg" id="full-screen-background-image" /> -->
 <div id='login_form' class="container">
 
-  <div class="span3"></div>
-  <div class="span5">
-    <br><br>
-    <div class="row">
-      <br><br><br><br><br><br><br>
-      <center><img src="<?= base_url() ?>assets/img/logo.png"></img></center>
-      <div class="login-box">
+    <div class="span3"></div>
+    <div class="span5">
+        <br><br>
+        <div class="row">
+            <br><br><br><br><br><br><br>
+            <center><img src="<?= base_url() ?>assets/img/logo.png"></img></center>
+            <div class="login-box">
 
-        <?php echo form_open(base_url() . 'login/', array('id' => 'loginForm')); ?>
-        <h3 class="form-signin-heading" style="color:white;">Please Sign-in</h3>
+                <?php echo form_open(base_url() . 'login/'); ?>
 
-        <div class="form-group">
-          <?php echo form_input(array('id' => 'username', 'name' => 'username', 'class' => 'h3 col-xs-12', 'placeholder' => 'Username')); ?>
-        </div>
-        <div class="form-group">
-          <?php echo form_password(array('id' => 'password', 'name' => 'password', 'class' => 'h3 col-xs-12', 'placeholder' => 'Password')); ?>
-        </div>
-        <div class="form-group">
-          <?php echo form_submit(array('name' => 'submit', 'class' => 'h3 btn-success btn-login btn-block', 'style' => 'height:40px'), 'Login'); ?>
-        </div>
+                <h3 class="form-signin-heading" style="color:white;">Please Sign-in</h3>
 
-        <?php echo form_close(); ?>
-        <br>
-      </div>
+                <?php
+                echo form_input(array('id' => 'username', 'name' => 'username', 'class' => 'h3 col-xs-12', 'placeholder' => 'Username'));
+                echo form_password(array('id' => 'password', 'name' => 'password', 'class' => 'h3 col-xs-12', 'placeholder' => 'Password'));
+                echo form_submit(array('name' => 'submit', 'class' => 'h3 btn-success btn-login btn-block', 'style' => 'height:40px'), 'Login');
+
+                if ($this->session->flashdata('login_error')) {
+                    echo '<br><center>You\'ve entered an incorrect username or password</center>';
+                }
+                echo validation_errors();
+                echo form_close();
+                ?>
+                <br>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <body>
--- application/views/loginfooter.php
+++ application/views/loginfooter.php
@@ -1,88 +1,38 @@
 </body>
 
-<!-- Le javascript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="<?= base_url() ?>assets/js/bootstrap-timepicker.min.js"></script>
-<script src="<?= base_url() ?>assets/js/jquery-2.0.3.min.js"></script>
-<script src="<?= base_url() ?>assets/js/jquery.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-transition.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-alert.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-modal.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-dropdown.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-scrollspy.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-tab.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-tooltip.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-popover.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-button.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-collapse.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-carousel.js"></script>
-<script src="<?= base_url() ?>assets/js/bootstrap-typeahead.js"></script>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?=base_url() ?>assets/js/bootstrap-timepicker.min.js"></script>
+    <script src="<?=base_url() ?>assets/js/jquery-2.0.3.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-transition.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-alert.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-modal.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-tab.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-popover.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-button.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-collapse.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-carousel.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap-typeahead.js"></script>
 
-<script src="<?= base_url() ?>assets/js/qunit.js"></script>
-<script src="<?= base_url() ?>assets/js/jquery-loader.js"></script>
-<script src="<?= base_url() ?>assets/js/jquery-backstretch.js"></script>
-<script src="<?= base_url() ?>assets/js/jquery.backstretch.js"></script>
-<script src="<?= base_url() ?>/assets/js/jquery.backstretch.min.js"></script>
-
-<script type="text/javascript" src="<?= base_url() ?>assets/bootstrapvalidator/dist/js/bootstrapValidator.min.js"></script>
-
-<script>
-  $.backstretch([
-    "<?= base_url() ?>assets/img/02.png",
-    "<?= base_url() ?>assets/img/01.png",
-    "<?= base_url() ?>assets/img/03.png"
-  ], {
-    fade: 750,
-    duration: 4000
-  });
-</script>
-
-<script type='text/javascript'>
-  $(document).ready(function() {
-    $('#loginForm').bootstrapValidator({
-      message: 'This value is not valid',
-      feedbackIcons: {
-        valid: 'glyphicon glyphicon-ok',
-        invalid: 'glyphicon glyphicon-remove',
-        validating: 'glyphicon glyphicon-refresh'
-      },
-      fields: {
-        username: {
-          message: 'The username is not valid',
-          validators: {
-            notEmpty: {
-              message: 'The username is required and cannot be empty'
-            },
-            stringLength: {
-              min: 6,
-              max: 30,
-              message: 'The username must be more than 6 and less than 30 characters long'
-            },
-            regexp: {
-              regexp: /^[a-zA-Z0-9_]+$/,
-              message: 'The username can only consist of alphabetical, number and underscore'
-            }
-          }
-        },
-        password: {
-          validators: {
-            notEmpty: {
-              message: 'The password is required and cannot be empty'
-            },
-            different: {
-              field: 'username',
-              message: 'The password cannot be the same as username'
-            },
-            stringLength: {
-              min: 8,
-              message: 'The password must have at least 8 characters'
-            }
-          }
-        }
-      }
-    });
-  });
-</script>
+    <script src="<?=base_url()?>assets/js/qunit.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery-loader.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery-backstretch.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery.backstretch.js"></script>
+    <script src="<?=base_url() ?>/assets/js/jquery.backstretch.min.js"></script>
+    <script>
+        $.backstretch([
+          "<?=base_url()?>assets/img/02.png",
+          "<?=base_url()?>assets/img/01.png",
+          "<?=base_url()?>assets/img/03.png"
+          ], {
+            fade: 750,
+            duration: 4000
+        });
+    </script>
 
 </html>
